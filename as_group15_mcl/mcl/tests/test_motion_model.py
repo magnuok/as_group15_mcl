8aa1f8b9455565fc62220feae20474707f1138a6
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2018-11-10 17:33:47 +0000

    adds test folder, adds implementation of motion_odel

--- as_group15_mcl/mcl/tests/test_motion_model.py
+++ as_group15_mcl/mcl/tests/test_motion_model.py
@@ -1,3 +1,7 @@
 """
 Just example of how we should structure the tests
 """
+
+# TODO: make test for checking whether motion_model returns something
+# TODO: make test that checks that the returned values are within the expected values
+# TODO: make test that checks that it returns the values quick enough
