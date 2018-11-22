37d2f246b358385f12ea6b323d3379dbdf9c6423
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2018-11-22 13:26:23 +0000

    adds initialize functions, adds loop ..

--- src/monte_carlo/src/monte_carlo.py
+++ src/monte_carlo/src/monte_carlo.py
@@ -10,6 +10,7 @@ from sensor_msgs.msg import LaserScan
 import random
 import math
 import numpy
+import time  # TEST
 
 
 class MonteCarlo:
@@ -17,54 +18,48 @@ class MonteCarlo:
     Class description
     """
 
+    # TODO: maybe remove these, put them in __init__
     _laser_point_list = []  # List with laserscan. Scanning [pi, 0]. 512 scans
     _odometry = ()  # Contains the new odometry tuppel = (x,y,theta)
     _map = []  # Contains list of cells in map
     _particles = []  # List with particle tuples = (x, y, theta)
     _pose_array = PoseArray()
-    _occupancy_grid_msg = OccupancyGrid()  # TEST
+    _occupancy_grid_msg = OccupancyGrid()
+    publisher = None
 
-    def callback_odometry(self, odom_msg):
-        """
-        callback function used by a ros subscriber. Receives the odometry and saves it to a list.
-        :param pose: ros pose
-        """
-        x = odom_msg.pose.pose.position.x
-        y = odom_msg.pose.pose.position.y
-        # Transform quaternion to euler
-        quaternion = odom_msg.pose.pose.orientation
-        euler = tf.transformations.euler_from_quaternion([quaternion.x, quaternion.y, quaternion.z, quaternion.w])
-        self._odometry = (x, y, euler[2])
-        # rospy.loginfo(self._odometry)
+    _new_odometry = False
+    _new_laser_data = False
 
-        # self._odometry = pose
+    def __init__(self):
+        # Initialize mcl node
+        rospy.init_node('monte_carlo', anonymous=True)
 
-        # rospy.loginfo("Received Odom pose:")
-        # rospy.loginfo("Timestamp" + str(self._odometry.header.stamp))
-        # rospy.loginfo("frame_id:" + str(self._odometry.header.frame_id))
-        #
-        # # Copying for simplicity
-        # position = self._odometry.pose.pose.position
-        # quat = self._odometry.pose.pose.orientation
-        # rospy.loginfo("Point Position: [ %f, %f, %f ]" % (position.x, position.y, position.z))
-        # rospy.loginfo("Quat Orientation: [ %f, %f, %f, %f]" % (quat.x, quat.y, quat.z, quat.w))
-        #
-        # # Also print Roll, Pitch, Yaw
-        # euler = tf.transformations.euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
-        # rospy.loginfo("Euler Angles: %s" % str(euler))
-        # rospy.loginfo("")
+        # initializes particles and publisher
+        self._initialize_particles()
+        self._initialize_publisher()
+        self._initialize_subscribers()
 
-    def callback_laser(self, laserscan):
-        """
-        callback function used by a ros subscriber. Receives the laser and saves it to a list.
-        :param pose: ros massage
-        """
-        self._laser_point_list = laserscan.ranges
-        # rospy.loginfo(rospy.get_caller_id() + "I heard %s", laserscan)
-        # rospy.loginfo("%s", self._laser_point_list)
-        # rospy.loginfo("%s", len(self._laser_point_list))
+        # Set frame_id in pose_array to be recognized in rviz
+        self._pose_array.header.frame_id = "map"
 
-    def _calculate_new_particle_list(self, old_particles, odometry, laser_points, map):
+    def loop(self):
+
+        # rate = rospy.Rate(1) #Every 1/frequency seconds. Input freq
+
+        # While not mcl is terminated by user
+        while not rospy.is_shutdown():
+            start_time = time.time()  # start time of loop [seconds]
+            if self._new_odometry and self._new_laser_data:
+                self._particles = self._update_particle_list(self._particles, self._odometry, self._laser_point_list,
+                                                             self._map)
+                self._pose_array = self._update_pose_array(self._particles)
+                self._publish_pose_array()
+
+        # rate.sleep() run loop at exact time in Hz
+        elapsed_time = time.time() - start_time
+        rospy.loginfo("Loop time:" + str(elapsed_time))
+
+    def _update_particle_list(self, old_particles, odometry, laser_points, map):
         """
         calculates new particles based on Monte Carlo Localization.
         :param old_particles: The old list of particles
@@ -76,23 +71,23 @@ class MonteCarlo:
         # the new particles
         particle_list = []
         # the old_particles after applying the motion_model to them
-        pose_list = []
+        predicted_particles_list = []
         # the weights of the old_particles after applying the motion_model to them
         weight_list = []
 
         # motion and measurement model
         for particle in old_particles:
             # get new poses after applying the motion_model
-            pose_list.append(type(self).sample_motion_model_odometry(odometry, particle))
+            predicted_particles_list.append(type(self).sample_motion_model_odometry(odometry, particle))
 
             # get weights corresponding to the new pose_list
-            weight_list.append(type(self).measurement_model(laser_points, pose_list, map))
+            weight_list.append(type(self).measurement_model(laser_points, predicted_particles_list, map))
 
         # sample the new particles
-        particle_list = type(self).low_variance_sampler(pose_list, weight_list)
+        particle_list = type(self).low_variance_sampler(predicted_particles_list, weight_list)
 
         # return the new set of particles
-        self._particles = particle_list
+        return particle_list
 
     @classmethod
     def sample_motion_model_odometry(cls, odometry, x_last):
@@ -153,31 +148,31 @@ class MonteCarlo:
 
     @staticmethod
     def low_variance_sampler(particles, weights):
+        """
+        :param particles:
+        :param weights:
+        :return:
+        """
         new_particle_list = []
-        r = random.random()*(1./(len(particles)))
+        r = random.random() * (1. / (len(particles)))
         c = weights[0]
         i = 0
 
-        for m in range(1,len(particles)+1):
-            u = r + (m-1)*(1./(len(particles)))
+        for m in range(1, len(particles) + 1):
+            u = r + (m - 1) * (1. / (len(particles)))
             while u > c:
-              i += 1
-              c += weights[i]
+                i += 1
+                c += weights[i]
             new_particle_list.append(particles[i])
         return new_particle_list
 
-    def callback_map(self, occupancy_grid_msg):
-        """
+    def _update_pose_array(self, _particles):
+        for particle in self._particles:
+            pose = self._create_pose(particle)
+            self._pose_array.poses.append(pose)  # Add to pose_array
+        pass
 
-        :param occupancy_grid_msg:
-        :return:
-        """
-        self._map = occupancy_grid_msg.data  # Total map
-
-        rospy.loginfo("MAP: " + str(self._map))
-        self._occupancy_grid_msg = occupancy_grid_msg
-
-    def initialize_particles(self):
+    def _initialize_particles(self):
         """
 
         :param:
@@ -234,21 +229,22 @@ class MonteCarlo:
             self._particles.append(
                 (particle_height, particle_width, random.uniform(0, 2 * math.pi)))
 
-    def publish_pose_array(self):
-        pub = rospy.Publisher('/PoseArray', PoseArray, queue_size=10)
-        rate = rospy.Rate(2)  # Every 1/f seconds
+    def _initialize_publisher(self):
+        # initialize the publisher object
+        self.publisher = rospy.Publisher('/PoseArray', PoseArray, queue_size=10)
 
-        self._pose_array.header.frame_id = "map"  # Set frame_id to be recognized in rviz
+    def _initialize_subscribers(self):
+        rospy.Subscriber("/map", OccupancyGrid, mcl.callback_map)
+        rospy.Subscriber("/RosAria/pose", Odometry, mcl.callback_odometry)
+        rospy.Subscriber("/scan", LaserScan, mcl.callback_laser)
 
-        # While not shutdown: publish a PoseArray containing particles to rviz
-        while not rospy.is_shutdown():
-            # Create new pose from article to publish to rviz
-            for particle in self._particles:
-                pose = self._create_pose(particle)
-                self._pose_array.poses.append(pose)  # Add to pose_array
+    def _publish_pose_array(self):
+        """
+        :return:
+        """
 
-            pub.publish(self._pose_array)
-            rate.sleep()
+        # publishes the particles
+        self.publisher.publish(self._pose_array)
 
     def _create_pose(self, particle):
         pose = Pose()
@@ -263,6 +259,59 @@ class MonteCarlo:
         pose.orientation.z = quaternion[2]
         pose.orientation.w = quaternion[3]
         return pose
+
+    def callback_odometry(self, odom_msg):
+        """
+        callback function used by a ros subscriber. Receives the odometry and saves it to a list.
+        :param pose: ros pose
+        """
+        x = odom_msg.pose.pose.position.x
+        y = odom_msg.pose.pose.position.y
+        # Transform quaternion to euler
+        quaternion = odom_msg.pose.pose.orientation
+        euler = tf.transformations.euler_from_quaternion([quaternion.x, quaternion.y, quaternion.z, quaternion.w])
+        self._odometry = (x, y, euler[2])
+
+
+        # rospy.loginfo(self._odometry)
+
+        # self._odometry = pose
+
+        # rospy.loginfo("Received Odom pose:")
+        # rospy.loginfo("Timestamp" + str(self._odometry.header.stamp))
+        # rospy.loginfo("frame_id:" + str(self._odometry.header.frame_id))
+        #
+        # # Copying for simplicity
+        # position = self._odometry.pose.pose.position
+        # quat = self._odometry.pose.pose.orientation
+        # rospy.loginfo("Point Position: [ %f, %f, %f ]" % (position.x, position.y, position.z))
+        # rospy.loginfo("Quat Orientation: [ %f, %f, %f, %f]" % (quat.x, quat.y, quat.z, quat.w))
+        #
+        # # Also print Roll, Pitch, Yaw
+        # euler = tf.transformations.euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
+        # rospy.loginfo("Euler Angles: %s" % str(euler))
+        # rospy.loginfo("")
+
+    def callback_laser(self, laserscan):
+        """
+        callback function used by a ros subscriber. Receives the laser and saves it to a list.
+        :param pose: ros massage
+        """
+        self._laser_point_list = laserscan.ranges
+        # rospy.loginfo(rospy.get_caller_id() + "I heard %s", laserscan)
+        # rospy.loginfo("%s", self._laser_point_list)
+        # rospy.loginfo("%s", len(self._laser_point_list))
+
+    def callback_map(self, occupancy_grid_msg):
+        """
+
+        :param occupancy_grid_msg:
+        :return:
+        """
+        self._map = occupancy_grid_msg.data  # Total map
+
+        # rospy.loginfo("MAP: " + str(self._map))
+        self._occupancy_grid_msg = occupancy_grid_msg
 
 
 if __name__ == '__main__':
