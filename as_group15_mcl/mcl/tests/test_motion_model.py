c018b4ea3489139237284ce0f99006e22d063c1e
Magnus Knaedal <magnusknaedal@gmail.com>
2018-11-22 13:57:47 +0000

    removes old project

--- as_group15_mcl/mcl/tests/test_motion_model.py
+++
@@ -1,7 +0,0 @@
-"""
-Just example of how we should structure the tests
-"""
-
-# TODO: make test for checking whether motion_model returns something
-# TODO: make test that checks that the returned values are within the expected values
-# TODO: make test that checks that it returns the values quick enough
