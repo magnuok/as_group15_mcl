d9d17b501e21ddb97103c3653ef90f7cb3cb379d
Joakim Hegg Johansen <joakimjohansen1996@gmail.com>
2018-11-23 16:46:19 +0000

    adds tests to sample_motion_model

--- src/monte_carlo/tests/test_sample_motion_model_odometry.py
+++ src/monte_carlo/tests/test_sample_motion_model_odometry.py
@@ -6,7 +6,7 @@ class Test_sample_motion_model_odometry(unittest.TestCase):
 
 
     def setUp(self):
-        self.mcl = MonteCarlo()
+        self.mcl = MonteCarlo(testing=True)
         self.odometry_list = [(0,0,math.pi), (1, 1, math.pi/2)]
         self.particle_list = [(1,1, 2), (1, 5, math.pi), (22, 51515, 0)]
 
@@ -21,6 +21,12 @@ class Test_sample_motion_model_odometry(unittest.TestCase):
                 self.assertNotEqual(MonteCarlo.sample_motion_model_odometry(odometry, particle), None)
 
 
+    def test_receives_correct_format(self):
+        for odometry in self.odometry_list:
+            for particle in self.particle_list:
+                self.assertTrue(len(MonteCarlo.sample_motion_model_odometry(odometry, particle)), 3)
+
+    # TODO: add test that check that the retuned angle is between 0 and 2pi (?)
 
 if __name__ == '__main__':
     unittest.main()
