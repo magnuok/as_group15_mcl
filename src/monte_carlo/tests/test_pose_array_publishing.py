496142e11908d07ff73bb7e29642ea8f0f38d48a
Magnus Knaedal <magnusknaedal@gmail.com>
2018-11-26 15:33:17 +0000

    unit test for pose_array_publishing

--- src/monte_carlo/tests/test_pose_array_publishing.py
+++ src/monte_carlo/tests/test_pose_array_publishing.py
@@ -1,24 +1,36 @@
 import unittest
 from src.monte_carlo import MonteCarlo
 import math
+import time
 
 class Test_pose_array_publishing(unittest.TestCase):
 
 
     def setUp(self):
         self.mcl = MonteCarlo(testing=True)
+        self.mcl._initialize_publisher()
+
+        for i in range (0,100):
+            particle = (1 ,1 ,math.pi)
+            self.mcl._particles.append(particle)
 
 
-        self.particle_list = [(1,1, 2), (1, 5, math.pi), (22, 51515, 0)]
-        self.mcl._update_pose_array(self.particle_list)
 
-
+    def test_response_time(self):
+        self.mcl._update_pose_array(self.mcl._particles)
+        start_time = time.time()
+        self.mcl._publish_pose_array(self.mcl._pose_array)
+        elapsed_time = time.time() - start_time
+        while elapsed_time < 2:
+            elapsed_time = time.time() - start_time
+        self.assertTrue(elapsed_time<10)
 
     def tearDown(self):
         pass
 
     def test_receive_same_amount_of_particles(self):
-        self.assertTrue(MonteCarlo.low_variance_sampler(predicted_particles=self.particle_list, weights=self.weights))
+        pass
+        # self.assertTrue()
 
 
     # TODO: what other test do we need?
