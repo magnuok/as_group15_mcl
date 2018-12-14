7bfe66dc3e14eec2b8a49c2b9495fc85ac2774af
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2018-12-14 10:42:54 +0000

    adds matlab files

---
+++ matlab/analysis.m
@@ -0,0 +1,9 @@
+information = load('../run_monte_carlo_files/information1544781224.3.txt');
+mocap_pose_after_iteration = load('../run_monte_carlo_files/_mocap_pose_after_iteration1544781224.3.txt');
+mocap_pose_before_iteration = load('../run_monte_carlo_files/_mocap_pose_before_iteration1544781224.3.txt');
+particle_list_after_resampling = load('../run_monte_carlo_files/_particle_list_after_resampling1544781224.3.txt');
+particle_list_before_resampling = load('../run_monte_carlo_files/_particle_list_before_resampling1544781224.3.txt');
+weights = load('../run_monte_carlo_files/_weights.txt1544781224.3.txt');
+iteration_time = load('../run_monte_carlo_files/iteration_time.txt1544781224.3.txt');
+
+
