7cd043dd10a30e2a641f9afd28f15a5af92b7613
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2018-12-15 13:26:15 +0000

    Done the read + plot to several runs

--- matlab/analysis.m
+++ matlab/analysis.m
@@ -1,20 +1,19 @@
-close all;
-
-number = 1544790720.89;
-
-information = load(['../run_monte_carlo_files/information',num2str(number),'.txt']);
-mocap_pose_after_iteration = load(['../run_monte_carlo_files/_mocap_pose_after_iteration',num2str(number),'.txt']);
-mocap_pose_before_iteration = load(['../run_monte_carlo_files/_mocap_pose_before_iteration',num2str(number),'.txt']);
-particle_list_after_resampling = load(['../run_monte_carlo_files/_particle_list_after_resampling',num2str(number),'.txt']);
-particle_list_before_resampling = load(['../run_monte_carlo_files/_particle_list_before_resampling',num2str(number),'.txt']);
-weights = load(['../run_monte_carlo_files/_weights',num2str(number),'.txt']);
-iteration_time = load(['../run_monte_carlo_files/iteration_time',num2str(number),'.txt']);
-
-number_of_particles = information(1);
-loop_time = information(3);
-
+% close all;
+% 
+% number = 1544790720.89;
+% 
+% information = load(['../run_monte_carlo_files/information',num2str(number),'.txt']);
+% mocap_pose_after_iteration = load(['../run_monte_carlo_files/_mocap_pose_after_iteration',num2str(number),'.txt']);
+% mocap_pose_before_iteration = load(['../run_monte_carlo_files/_mocap_pose_before_iteration',num2str(number),'.txt']);
+% particle_list_after_resampling = load(['../run_monte_carlo_files/_particle_list_after_resampling',num2str(number),'.txt']);
+% particle_list_before_resampling = load(['../run_monte_carlo_files/_particle_list_before_resampling',num2str(number),'.txt']);
+% weights = load(['../run_monte_carlo_files/_weights',num2str(number),'.txt']);
+% iteration_time = load(['../run_monte_carlo_files/iteration_time',num2str(number),'.txt']);
+% 
+% number_of_particles = information(1);
+% loop_time = information(3);
+% 
 %% Define the estimated position from particle list 
-
 % Position estimation based on the ten highest weights
 estimated_position = [];
 stop = number_of_particles;
@@ -119,15 +118,13 @@ iteration_time_var = var(iteration_time);
 %set(gca,'fontsize',16,'box','off')
 
 
-%% historgram of iteration time
+%% plot historgram of iteration time
 figure
 histogram(iteration_time,0:0.1:2)
 title('Iteration time of MCL')
 xlabel('count')
 ylabel('iteration time')
 set(gca,'fontsize',16,'box','off','ygrid', 'on' )
-
-
 
 %% plot the movement of the particles
 
@@ -146,58 +143,160 @@ for k = 1:length(estimated_position)
 end
 
 %% Calculate and plot error for multiple runs 
+clear all
 
-close all;
+% Load data
+information = [];
+mocap_pose_after_iteration = [];
+mocap_pose_before_iteration = [];
+particle_list_after_resampling = [];
+particle_list_before_resampling = [];
+weights = [];
+iteration_time = [];
 
-
-% Make a 3-by-8 matrix of 9s:
-
-information(1:10,1:5) = 0;
-mocap_pose_after_iteration(1:1000,1:5) = 0;
-mocap_pose_before_iteration(1:1000,1:5) = 0;
-particle_list_after_resampling(1:1000,1:5) = 0;
-particle_list_before_resampling(1:1000,1:5) = 0;
-weights(1:1000,1:5) = 0;
-iteration_time(1:1000,1:5) = 0;
-
+% Change here numbers
 numbers = [1544814756.35, 1544814933.88, 1544815107.57, 1544815335.77, 1544815521.67, 1544863129.47];
 
 for i = 1:5
     number = numbers(i);
-    information(i) = load(['../run_monte_carlo_files/testing_box_1_meter_around/information',num2str(number),'.txt']);
-    mocap_pose_after_iteration(i) = load(['../run_monte_carlo_files/testing_box_1_meter_around/_mocap_pose_after_iteration',num2str(number),'.txt']);
-    
-    
-    
-    mocap_pose_after_iteration = [mocap_pose_after_iteration, load(['../run_monte_carlo_files/testing_box_1_meter_around/_mocap_pose_after_iteration',num2str(number),'.txt'])];
-    %mocap_pose_before_iteration = [mocap_pose_before_iteration, load(['../run_monte_carlo_files/testing_box_1_meter_around/_mocap_pose_before_iteration',num2str(number),'.txt'])];
-    %particle_list_after_resampling = [particle_list_after_resampling, load(['../run_monte_carlo_files/testing_box_1_meter_around/_particle_list_after_resampling',num2str(number),'.txt'])];
-    %particle_list_before_resampling = [particle_list_before_resampling, load(['../run_monte_carlo_files/testing_box_1_meter_around/_particle_list_before_resampling',num2str(number),'.txt'])];
-    %weights = [weights, load(['../run_monte_carlo_files/testing_box_1_meter_around/_weights',num2str(number),'.txt'])];
-    %iteration_time = [iteration_time, load(['../run_monte_carlo_files/testing_box_1_meter_around/iteration_time',num2str(number),'.txt'])];
+    information = [information, load(['../run_monte_carlo_files/testing_box_1_meter_around/information',num2str(number),'.txt'])];
+end
 
-    number_of_particles = information(1);
-    loop_time = information(3);
+number_of_particles = information(1);
+loop_time = information(3);
+% Change here the min length of mocap and particles
+min_len_mocap = 300; 
+min_len_particles = min_len_mocap*number_of_particles;
 
+for i = 1:5
+    
+    number = numbers(i);
+    mocap_pose_after_iteration_load = load(['../run_monte_carlo_files/testing_box_1_meter_around/_mocap_pose_after_iteration',num2str(number),'.txt']);
+    mocap_pose_after_iteration = [mocap_pose_after_iteration, mocap_pose_after_iteration_load(1:min_len_mocap,1:3)];
+    
+    mocap_pose_before_iteration_load = load(['../run_monte_carlo_files/testing_box_1_meter_around/_mocap_pose_before_iteration',num2str(number),'.txt']);
+    mocap_pose_before_iteration = [mocap_pose_before_iteration, mocap_pose_before_iteration_load(1:min_len_mocap,1:3)];
+    
+    particle_list_after_resampling_load = load(['../run_monte_carlo_files/testing_box_1_meter_around/_particle_list_after_resampling',num2str(number),'.txt']);
+    particle_list_after_resampling = [particle_list_after_resampling, particle_list_after_resampling_load(1:min_len_particles,1:3)];
+    
+    particle_list_before_resampling_load = load(['../run_monte_carlo_files/testing_box_1_meter_around/_particle_list_before_resampling',num2str(number),'.txt']);
+    particle_list_before_resampling = [particle_list_before_resampling, particle_list_before_resampling_load(1:min_len_particles,1:3)];
+
+    weights_load = load(['../run_monte_carlo_files/testing_box_1_meter_around/_weights',num2str(number),'.txt']);
+    weights = [weights, weights_load(1:min_len_particles)];
+    
+    iteration_time_load = load(['../run_monte_carlo_files/testing_box_1_meter_around/iteration_time',num2str(number),'.txt']);
+    iteration_time = [iteration_time, iteration_time_load(1:min_len_mocap)];
+end
+
+% Position estimation based on the ten highest weights
+estimated_position = [];
+
+for i=1:number_of_particles:length(weights)
+    [val, ind] = sort(weights(i:i+49,:),'descend');
+    pose = mean(particle_list_before_resampling(ind(1:10)+i-1,:));
+    estimated_position = [estimated_position; pose];
+end
+
+% Calculate error
+error = mocap_pose_before_iteration - estimated_position;
+pose_error_x = [];
+pose_error_y = [];
+orientation_error = [];
+
+for i=1:3:length(numbers)*3-3
+    pose_error_x = [pose_error_x, error(:,i)];
+end
+for i=2:3:length(numbers)*3-3
+    pose_error_y = [pose_error_y, error(:,i)];
+end    
+for i=3:3:length(numbers)*3-3
+    orientation_error = [orientation_error, error(:,i)];
+end    
+pose_error = sqrt((pose_error_x.^2 + pose_error_y.^2));
+
+% Calculate mean of all errors
+pose_error_x_mean = [];
+pose_error_y_mean = [];
+pose_error_mean = [];
+orientation_error_mean = [];
+
+
+for i=1:length(pose_error)
+    pose_error_x_mean = [pose_error_x_mean, mean(pose_error_x(i,:))];
+    pose_error_y_mean = [pose_error_y_mean, mean(pose_error_y(i,:))];
+    pose_error_mean = [pose_error_mean, mean(pose_error(i,:))];
+    orientation_error_mean = [orientation_error_mean, mean(orientation_error(i,:))];
+end
+
+a = 'ok'
+
+%% plot mean error of all runs 
+
+t = transpose(0:loop_time:1000);
+
+figure
+plot(t(1:length(error)),pose_error_mean(1:length(error)),'+','LineWidth', 2)
+set(gca,'fontsize',16,'box','off')
+title('Mean of the pose error, 5 runs, 1m box')
+xlabel('time [s]')
+ylabel ('error [m]')
+
+figure
+plot(t(1:length(error)),pose_error_x_mean(1:length(error)),'+','LineWidth', 2)
+set(gca,'fontsize',16,'box','off')
+title('Mean of the pose error x, 5 runs, 1m box')
+xlabel('time [s]')
+ylabel ('error [m]')
+
+figure
+plot(t(1:length(error)),pose_error_y_mean(1:length(error)), '+','LineWidth', 2)
+set(gca,'fontsize',16,'box','off')
+title('Mean of the pose error y, 5 runs, 1m box')
+xlabel('time [s]')
+ylabel ('error [m]')
+
+figure
+plot(t(1:length(error)),orientation_error_mean(1:length(error)), '+', 'LineWidth', 2)
+set(gca,'fontsize',16,'box','off')
+title('Mean of the orientation error, 5 runs, 1m box')
+xlabel('time [s]')
+ylabel ('error [rad]')
+
+%% 
+
+figure
+for i=1:5
+    figure
+    plot(t(1:length(error)),pose_error_x(1:length(error),i))
+    set(gca,'fontsize',16,'box','off')
+    title('pose error x 5 runs, 1m box')
+    xlabel('time [s]')
+    ylabel ('error [rad]')
+    hold on
 end
 
 
 
-pose_error_mean = mean(pose_error1, pose_error2, pose_error3, pose_error4, pose_error5);
+
+
+
+%% plot mean error of all runs 
+
+t = transpose(0:loop_time:1000);
 
 figure
-plot(t(1:length(error)),pose_error_mean(1:length(error)),'LineWidth', 1)
+plot(t(1:length(error)),pose_error_mean(1:length(error)))
 set(gca,'fontsize',16,'box','off')
-title('Pose error mean with 1 m box')
+title('Mean of the pose error, 5 runs, 1m box')
 xlabel('time [s]')
 ylabel ('error [m]')
 
-hold on
-boxplot([pose_error1(1), pose_error2(1), pose_error3(1), pose_error4(1), pose_error5(1)])
-set(gca,'fontsize',16,'box','off')
+axis([0, 100, 0, 3])
 
+hold on 
 
-
-
+%% Calculate meadian of the x anf y mean errors 
 
 
